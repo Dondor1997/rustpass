fcca174c0c157a0a3e9211dad74b7ca1c5807aaa
Dominik Trippel <dominik-trippel@arcor.de>
2019-07-10 01:04:22 +0200

    added general info for clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rustpass")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("command line password manager")
+        .arg(Arg::with_name("v")
+             .short("v")
+             .multiple(true)
+             .help("Sets the level of verbosity"))
+        .get_matches();
 }
