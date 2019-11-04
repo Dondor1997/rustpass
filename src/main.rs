73be9cae3da36c551f075e6baf180f4a27014961
Dominik <dtrippel@mail.upb.de>
2019-11-04 13:24:23 +0100

    redid user interface

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate clap;
 extern crate dirs;
 extern crate rpassword;
 
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand};
 use std::path::PathBuf;
 use std::collections::HashMap;
 use entry::Entry;
@@ -39,24 +39,54 @@ fn main() {
              .long("path")
              .help("sets alternative database path")
              .takes_value(true)
-             .value_name("path"))
+             .value_name("PATH"))
+        .arg(Arg::with_name("config")
+             .short("c")
+             .long("config")
+             .help("sets the config file")
+             .takes_value(true)
+             .value_name("CONFIG"))
         .arg(Arg::with_name("v")
              .short("v")
              .multiple(true)
              .help("Sets the level of verbosity"))
-        .arg(Arg::with_name("mode")
-             .help("select behaviour")
-             .required(true)
-             .index(1)
-             .possible_values(&["insert", "delete", "get", "init", "search", "list"]))
-        .arg(Arg::with_name("key")
-             .help("key to be modified")
-             .required(true)
-             .index(2))
+        .subcommand(SubCommand::with_name("insert")
+                    .about("insert new database entry")
+                    .arg(Arg::with_name("entry")
+                         .takes_value(true)
+                         .required(true)
+                         .value_name("ENTRY")))
+        .subcommand(SubCommand::with_name("delete")
+                    .about("delete a database entry")
+                    .arg(Arg::with_name("entry")
+                         .takes_value(true)
+                         .required(true)
+                         .value_name("ENTRY")))
+        .subcommand(SubCommand::with_name("get")
+                    .about("get an entry from the database")
+                    .arg(Arg::with_name("entry")
+                         .takes_value(true)
+                         .required(true)
+                         .value_name("ENTRY"))
+                    .arg(Arg::with_name("output")
+                         .short("o")
+                         .help("prints entry to stdout instead to clipboard")))
+        .subcommand(SubCommand::with_name("search")
+                    .about("search the database by entry name")
+                    .arg(Arg::with_name("entry")
+                         .takes_value(true)
+                         .required(true)
+                         .value_name("ENTRY")))
+        .subcommand(SubCommand::with_name("list")
+                    .about("print all database entrys")
+                    .arg(Arg::with_name("entry")
+                         .takes_value(true)
+                         .required(true)
+                         .value_name("ENTRY")))
         .get_matches();
 
     let mut default: PathBuf  = dirs::home_dir().unwrap();
-    default.push(".rustpass.db");
+    default.push(".local/share/rustpass/");
 
     let path = matches
         .value_of("path")
